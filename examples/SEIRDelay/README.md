ec7f8701d7922148496e103eded1f817c4f08588
liamblake <liamlemblake@gmail.com>
2025-04-02 16:44:50 +1030

    Add epinow_example config + readme

--- examples/SEIRDelay/README.md
+++ examples/SEIRDelay/README.md
@@ -1,5 +1,11 @@
 Simple SEIR model with delayed detections.
 
+There are three different config files:
+- `config.yaml`: simulates the model and uses the observations for inference
+
+- `config_flu2009`: uses `data/flu2009_incidence.csv`, which is the daily incidence of acute respiratory illness amongst children in a school during the 2009 H1N1 pandemic. Small number of cases. Based on [https://cran.r-project.org/web/packages/EpiEstim/vignettes/demo.html](), which provides some $R_0$ estimates to compare with.
+
+- `config_epinow`: uses `data/epinow_example.csv` which is a toy(?) dataset of notifications. The case numbers are very high, so the particle filter is probably a no-go for this one. Based on [https://epiforecasts.io/EpiNow2/articles/estimate_infections_workflow.html](), which accounts for reporting delays. The waiting time parameters in the config are set by taking the means of the relevant parameter distributions in the EpiNow2 example. The article also provides $R_0$ estimates to compare to.
 
 
 
--- examples/SEIRDelay/inference.jl
+++ examples/SEIRDelay/inference.jl
@@ -8,43 +8,46 @@ include("./utils/config.jl")
 include("./utils/io.jl")
 
 function main(argv)
-    argc = length(argv)
-    if argc != 1
-        error("inference.jl program expects 1 argument \
-               \n    - config file name.")
-    end
+	argc = length(argv)
+	if argc != 1
+		error("inference.jl program expects 1 argument \
+			   \n    - config file name.")
+	end
 
-    config = YAML.load_file(joinpath(pwd(), argv[1]))
+	config = YAML.load_file(joinpath(pwd(), argv[1]))
 
-    setenvironment!(config)
+	setenvironment!(config)
 
-    model, _ = makemodel(config)
+	model, _ = makemodel(config)
 
-    if config["inference"]["data"] == "simulated"
-        # If specified, use simulated data as observations
-        path, t = readparticles(joinpath(pwd(), config["simulation"]["outfilename"]))
-        cases = pathtodailycases(path, obs_state_idx(model.stateprocess))
-        observations = Observations(t, cases)
-    else
-        # Otherwise, assume "filename" and "first_observation_time" keys
-        raw_observations = read_observations(joinpath(pwd(), config["inference"]["data"]["filename"]))
-        t = config["inference"]["data"]["first_observation_time"] .+ (0:(length(raw_observations)-1))
-        observations = Observations(t, raw_observations)
-    end
+	if config["inference"]["data"] == "simulated"
+		# If specified, use simulated data as observations
+		path, t = readparticles(joinpath(pwd(), config["simulation"]["outfilename"]))
+		cases = pathtodailycases(path, obs_state_idx(model.stateprocess))
+		observations = Observations(t, cases)
+	else
+		# Otherwise, assume "filename" and "first_observation_time" keys
+		raw_observations =
+			read_observations(joinpath(pwd(), config["inference"]["data"]["filename"]))
+		t =
+			config["inference"]["data"]["first_observation_time"] .+
+			(0:(length(raw_observations)-1))
+		observations = Observations(t, raw_observations)
+	end
 
-    loglikelihood = makeloglikelihood(observations, config)
+	loglikelihood = makeloglikelihood(observations, config)
 
-    prior_logpdf = makeprior(config)
+	prior_logpdf = makeprior(config)
 
-    proposal_distribuion = makeproposal(config)
+	proposal_distribuion = makeproposal(config)
 
-    mh_rng, mh_config = makemhconfig(config)
+	mh_rng, mh_config = makemhconfig(config)
 
-    @time nsamples = MetropolisHastings.metropolis_hastings(
-        mh_rng, loglikelihood, prior_logpdf, proposal_distribuion, mh_config,
-    )
-    println()
-    return
+	@time nsamples = MetropolisHastings.metropolis_hastings(
+		mh_rng, loglikelihood, prior_logpdf, proposal_distribuion, mh_config,
+	)
+	println()
+	return
 end
 
 main(ARGS)
